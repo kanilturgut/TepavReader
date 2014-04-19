43c70f23806cccb811c7dd756d4c766b75b59fe4
Kadir Anıl Turğut <kanilturgut@gmail.com>
2014-04-20 00:36:00 +0300

    -

--- src/com/tepav/reader/activity/NewsDetails.java
+++ src/com/tepav/reader/activity/NewsDetails.java
@@ -3,7 +3,11 @@ package com.tepav.reader.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
 import com.tepav.reader.R;
+import com.tepav.reader.helpers.Util;
+import com.tepav.reader.model.News;
 
 /**
  * Author : kanilturgut
@@ -16,7 +20,19 @@ public class NewsDetails extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.context = this;
         setContentView(R.layout.activity_news_details);
+        this.context = this;
+
+        News news = (News) getIntent().getSerializableExtra("class");
+
+        WebView view = (WebView) findViewById(R.id.wvNewsDetailContentOfNews);
+        view.loadData(news.getHcontent(), "text/html; charset=UTF-8", null);
+
+        TextView baslik = (TextView) findViewById(R.id.tvNewsDetailTitleOfNews);
+        baslik.setText(news.getHtitle());
+
+
+        TextView time = (TextView) findViewById(R.id.tvNewsDetailTimeInformationOfNews);
+        time.setText(news.getHdate());
     }
 }

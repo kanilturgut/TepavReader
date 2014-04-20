61c94dbcdaa526f933e7640450df0c10a3ac1910
Kadir Anıl Turğut <kanilturgut@gmail.com>
2014-04-20 17:56:23 +0300

    refactoring

--- src/com/tepav/reader/fragment/NewsFragment.java
+++ src/com/tepav/reader/fragment/NewsFragment.java
@@ -1,7 +1,6 @@
 package com.tepav.reader.fragment;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;

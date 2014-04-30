38eeacd67955ee0a261c8f8bf74adeadfdf7be91
Kadir Anıl Turğut <kanilturgut@gmail.com>
2014-04-30 17:18:49 +0300

    View pager on Blog deleted

--- src/com/tepav/reader/fragment/BlogFragment.java
+++ src/com/tepav/reader/fragment/BlogFragment.java
@@ -8,21 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-import com.androidquery.AQuery;
-import com.androidquery.callback.AjaxCallback;
-import com.androidquery.callback.AjaxStatus;
 import com.tepav.reader.R;
 import com.tepav.reader.adapter.BlogListAdapter;
-import com.tepav.reader.adapter.BlogPagerAdapter;
-import com.tepav.reader.helpers.Constant;
-import com.tepav.reader.helpers.HttpURL;
-import com.tepav.reader.helpers.WrapContentHeightViewPager;
-import com.tepav.reader.helpers.pagerindicator.CirclePageIndicator;
 import com.tepav.reader.helpers.swipelistview.SwipeListView;
-import com.tepav.reader.model.Blog;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * Author : kanilturgut
@@ -32,14 +20,7 @@ import org.json.JSONObject;
 public class BlogFragment extends Fragment {
 
     Context context;
-
     SwipeListView swipeListViewOfBlog;
-    WrapContentHeightViewPager viewPagerOfBlog;
-    BlogPagerAdapter blogPagerAdapter;
-    CirclePageIndicator circlePageIndicator;
-
-    String[] urls = new String[Constant.DRAWERS_PAGE_NUMBER];
-
     RelativeLayout rlLoading;
 
     @Override
@@ -54,44 +35,12 @@ public class BlogFragment extends Fragment {
 
         rlLoading = (RelativeLayout) view.findViewById(R.id.rlLoading);
 
-        viewPagerOfBlog = (WrapContentHeightViewPager) view.findViewById(R.id.blogPager);
-        circlePageIndicator = (CirclePageIndicator) view.findViewById(R.id.circleIndicatorOfBlogPager);
-
         //swipe list view of blog
         swipeListViewOfBlog = (SwipeListView) view.findViewById(R.id.swipeListViewOfBlog);
         swipeListViewOfBlog.setAdapter(new BlogListAdapter(context, 1));
 
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("pageNumber", 1);
-        } catch (JSONException e) {
-            //this will return pageNumber:1
-            jsonObject = null;
-            e.printStackTrace();
-        }
-
-        AQuery aQuery = new AQuery(context);
-        aQuery.post(HttpURL.createURL(HttpURL.blog), jsonObject, JSONArray.class, new AjaxCallback<JSONArray>() {
-
-            @Override
-            public void callback(String url, JSONArray object, AjaxStatus status) {
-
-                for (int i = 0 ; i < Constant.DRAWERS_PAGE_NUMBER; i++) {
-                    try {
-                        urls[i] = Blog.fromJSON(object.getJSONObject(i)).getPimage();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                blogPagerAdapter = new BlogPagerAdapter(getFragmentManager(), context, urls);
-                viewPagerOfBlog.setAdapter(blogPagerAdapter);
-                circlePageIndicator.setViewPager(viewPagerOfBlog);
-
-                if (rlLoading != null)
-                    rlLoading.setVisibility(RelativeLayout.GONE);
-            }
-        });
+        if (rlLoading != null)
+        rlLoading.setVisibility(RelativeLayout.GONE);
 
         return view;
     }

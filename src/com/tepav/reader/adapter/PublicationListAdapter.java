43a109d04b53ad9c486de25bded4c951bcc57f2f
Kadir Anıl Turğut <kanilturgut@gmail.com>
2014-05-06 16:12:05 +0300

    Publication list

--- src/com/tepav/reader/adapter/PublicationListAdapter.java
+++ src/com/tepav/reader/adapter/PublicationListAdapter.java
@@ -14,11 +14,13 @@ import com.androidquery.AQuery;
 import com.androidquery.callback.AjaxCallback;
 import com.androidquery.callback.AjaxStatus;
 import com.tepav.reader.R;
+import com.tepav.reader.activity.BlogDetails;
 import com.tepav.reader.activity.PublicationDetails;
 import com.tepav.reader.db.DBHandler;
 import com.tepav.reader.helpers.Constant;
 import com.tepav.reader.helpers.HttpURL;
 import com.tepav.reader.helpers.Util;
+import com.tepav.reader.model.Blog;
 import com.tepav.reader.model.Publication;
 import com.tepav.reader.service.TepavService;
 import org.json.JSONArray;
@@ -119,79 +121,116 @@ public class PublicationListAdapter extends ArrayAdapter<Publication> {
         else
             holder.ibLike.setImageResource(R.drawable.swipe_like);
 
-        MyOnClickListener myOnClickListener = new MyOnClickListener(position);
-        holder.ibShare.setOnClickListener(myOnClickListener);
-        holder.ibFavorite.setOnClickListener(myOnClickListener);
-        holder.ibReadList.setOnClickListener(myOnClickListener);
-        holder.frontOfPublicationClick.setOnClickListener(myOnClickListener);
+        holder.ibShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = Constant.SHARE_NEWS + publication.getYayin_id();
 
-        return convertView;
-    }
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, publication.getYtitle());
+                shareIntent.putExtra(Intent.EXTRA_TEXT,  publication.getYtitle() + " " + url);
+                context.startActivity(Intent.createChooser(shareIntent, context.getString(R.string.share)));
+            }
+        });
 
-    class MyOnClickListener implements View.OnClickListener {
+        holder.ibFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        int position;
-
-        public MyOnClickListener(int pos) {
-            this.position = pos;
-        }
-
-        @Override
-        public void onClick(View view) {
-
-            Publication publication = publicationList.get(position);
-
-            switch (view.getId()) {
-                case R.id.ibShare:
-                    String url = Constant.SHARE_NEWS + publication.getYayin_id();
-
-                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                    shareIntent.setType("text/plain");
-                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, publication.getYtitle());
-                    shareIntent.putExtra(Intent.EXTRA_TEXT,  publication.getYtitle() + " " + url);
-                    context.startActivity(Intent.createChooser(shareIntent, context.getString(R.string.share)));
-                    break;
-                case R.id.ibFavorite:
-
+                if (!isPressedFavorite) {
                     try {
                         dbHandler.insert(Publication.toDBData(publication), DBHandler.TABLE_FAVORITE);
+                        tepavService.addItemToFavoriteListOfTepavService(Publication.toDBData(publication));
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-
-                    break;
-                case R.id.ibFavorited:
-
+                } else {
                     try {
                         dbHandler.delete(Publication.toDBData(publication), DBHandler.TABLE_FAVORITE);
+                        tepavService.removeItemFromFavoriteListOfTepavService(Publication.toDBData(publication));
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
+                }
 
-                    break;
-                case R.id.ibReadList:
+                isPressedFavorite = !isPressedFavorite;
+                ImageButton imageButton = (ImageButton) view;
+                if (isPressedFavorite)
+                    imageButton.setImageResource(R.drawable.swipe_favorites_dolu);
+                else
+                    imageButton.setImageResource(R.drawable.swipe_favorites);
 
+            }
+        });
+
+        holder.ibLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!isPressedLike) {
+                    try {
+                        dbHandler.insert(Publication.toDBData(publication),DBHandler.TABLE_LIKE);
+                        tepavService.addItemToLikeListOfTepavService(Publication.toDBData(publication));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        dbHandler.delete(Publication.toDBData(publication),DBHandler.TABLE_LIKE);
+                        tepavService.removeItemFromLikeListOfTepavService(Publication.toDBData(publication));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                isPressedLike = !isPressedLike;
+                ImageButton imageButton = (ImageButton) view;
+                if (!isPressedLike)
+                    imageButton.setImageResource(R.drawable.swipe_like);
+                else
+                    imageButton.setImageResource(R.drawable.swipe_like_dolu);
+            }
+        });
+
+        holder.ibReadList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!isPressedReadList) {
                     try {
                         dbHandler.insert(Publication.toDBData(publication), DBHandler.TABLE_READ_LIST);
+                        tepavService.addItemToReadingListOfTepavService(Publication.toDBData(publication));
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    break;
-                case R.id.ibReadListed:
+                } else {
                     try {
                         dbHandler.delete(Publication.toDBData(publication), DBHandler.TABLE_READ_LIST);
+                        tepavService.removeItemFromReadingListOfTepavService(Publication.toDBData(publication));
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    break;
-                case R.id.frontOfPublicationClick:
+                }
 
-                    Intent intent = new Intent(context, PublicationDetails.class);
-                    intent.putExtra("class", publication);
-                    context.startActivity(intent);
-                    break;
+                isPressedReadList = !isPressedReadList;
+                ImageButton imageButton = (ImageButton) view;
+                if (isPressedReadList)
+                    imageButton.setImageResource(R.drawable.okudum_icon_dolu);
+                else
+                    imageButton.setImageResource(R.drawable.okudum_icon);
             }
-        }
+        });
+        holder.frontOfPublicationClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, PublicationDetails.class);
+                intent.putExtra("class", publication);
+                context.startActivity(intent);
+            }
+        });
+
+        return convertView;
     }
 
     class PublicationHolder {
@@ -247,6 +286,9 @@ public class PublicationListAdapter extends ArrayAdapter<Publication> {
 
                     pageNumber++;
                 }
+
+                if (progressDialog != null)
+                    progressDialog.dismiss();
             }
         });
     }
